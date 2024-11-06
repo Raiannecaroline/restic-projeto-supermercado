edd18b0997f7b4e2e2a3ebbe35fec7865b9b90bf
Raiane Caroline S Teixeira <raianecaroline.st@gmail.com>
2024-11-06 18:56:31 -0300

    Update: Atulizando e colocando as autenticações

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginAuthComponent } from './components/login-auth/login-auth.component';
+import { HomeComponent } from './core/home/home.component';
+import { AuthGuard } from '@auth0/auth0-angular';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginAuthComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '**', redirectTo: '/login'
+  }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
